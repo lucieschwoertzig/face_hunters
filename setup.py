c735cb9011641c86bd598371150016cac1b42fa4
lucieschwoertzig <lucieschw@gmail.com>
2022-04-10 18:55:46 +0200

    MANIFEST

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'face_hunters',         # How you named your package folder (MyLib)
   packages = ['face_hunters'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is an application developped by 4BIM INSA students to help you create a robot portait of your agressor.',   # Give a short description about your library
   author = 'G3',                   # Type in your name
@@ -19,7 +19,7 @@ setup(
           'matplotlib',
           'scikit-image',
           'sklearn',
-          'scipy',
+          'scipy'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
