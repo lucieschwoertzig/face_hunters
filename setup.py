eafe550f81565db857c6f209dc8b9cf4c273dcb3
lucieschwoertzig <lucieschw@gmail.com>
2022-04-10 17:52:36 +0200

    with setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'face_hunters',         # How you named your package folder (MyLib)
   packages = ['face_hunters'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is an application developped by 4BIM INSA students to help you create a robot portait of your agressor.',   # Give a short description about your library
   author = 'G3',                   # Type in your name
   author_email = 'lucieschw@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/lucieschwoertzig/face_hunters',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/lucieschwoertzig/face_hunters/archive/refs/tags/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/lucieschwoertzig/face_hunters/archive/refs/tags/v_4.tar.gz',    # I explain this later on
   keywords = ['PORTRAITS', 'AUTOENCODER', 'GENETIC'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
